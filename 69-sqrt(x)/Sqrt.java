636708e122b5a75a85372fefb12e0d6f9695e0ee
Shashidharak89 <shashidharak334@gmail.com>
2025-08-07 12:52:23 +0530

    Sqrt(x)

--- 69-sqrt(x)/Sqrt.java
+++ 69-sqrt(x)/Sqrt.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 class Sqrt{
-    public static void main(String[] args) {
+    public int findsqrt(int x){
+        int l=0,r=x,mid;
+        int ans=0;
+        while(l<=r){
+            mid=l+(r-1)/2;
+            if((long)mid*mid>x){
+                r=mid-1;
+            }
+            else if((long)mid*mid<x){
+                ans=mid;
+                l=mid+1;
+            }
+            else if((long)mid*mid==x){
+                return mid;
+            }
+        }
+        return ans;
         
+    }
+    public static void main(String[] args) {
+        Sqrt obj=new Sqrt();
+        System.out.println(obj.findsqrt(16));
     }
 }
