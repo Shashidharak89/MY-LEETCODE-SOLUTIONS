aa9e21a75fa2d01e11b54342bcbf61f6487ee56a
Shashidharak89 <shashidharak334@gmail.com>
2025-09-15 23:02:22 +0530

    228. Summary Ranges
    [Top interview 150]

---
+++ 228-Summary-Ranges/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int i;
+        ArrayList<String> list=new ArrayList<>();
+        int n=nums.length;
+        for(i=0;i<nums.length;i++){
+            int start=nums[i];
+            while(i+1<n &&nums[i+1]==nums[i]+1){
+                i++;
+            }
+            if(start!=nums[i]){
+                list.add(""+start+"->"+nums[i]);
+            }
+            else{
+                list.add(""+start);
+            }
+
+           
+
+        }
+         return list;
+
+    }
+}
