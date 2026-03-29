4608d29cc4ccf85283b809ce79b3933df2965b18
Shashidhara K <shashidharak334@gmail.com>
2026-03-29 19:06:56 +0530

    3884. First Matching Character From Both Ends
    [Keep coding, Belated weekly contest problem]

---
+++ 3884-First-Matching-Character-From-Both-Ends/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int n=s.length();
+        if(n==1){
+            return 0;
+        }
+        for(int i=0;i<=n/2;i++){
+            if(s.charAt(i)==s.charAt(n-1-i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
