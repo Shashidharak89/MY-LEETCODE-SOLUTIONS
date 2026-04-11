d9c6ea3a5f45f8b0ef77bf2a2bc6ffb49b645160
Shashidhara K <shashidharak334@gmail.com>
2026-04-11 13:37:20 +0530

    3866. First Unique Even Element
    [Keep coding]

---
+++ 3866-First-Unique-Even-Element/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                if(map.containsKey(nums[i])){
+                    map.put(nums[i],map.get(nums[i])+1);
+                }
+                else{
+                    map.put(nums[i],1);
+                }
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                if(map.get(nums[i])==1){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
