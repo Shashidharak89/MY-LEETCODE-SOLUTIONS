c8cea5b2b260ed9f14706b1c7d6309d818bf45a2
Shashidhara K <shashidharak334@gmail.com>
2026-06-21 20:38:48 +0530

    1833. Maximum Ice Cream Bars
    [Keep coding, daily problem]

---
+++ 1833-Maximum-Ice-Cream-Bars/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i=0;i<costs.length;i++){
+            coins-=costs[i];
+            if(coins>=0){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
