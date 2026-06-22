ef07ffd6169598aff634adbe177e4c7e639c1e40
Shashidhara K <shashidharak334@gmail.com>
2026-06-22 12:48:36 +0530

    347. Top K Frequent Elements
    [Keep coding, Placement training Day 11]

---
+++ 347-Top-K-Frequent-Elements/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer a,Integer b){
+                return map.get(a)-map.get(b);
+            }
+        });
+        for(Integer ele: map.keySet()){
+            pq.add(ele);
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        int ans[]=new int[k];
+        int i=0;
+        for(Integer ele:pq){
+            ans[i++]=ele;
+        }
+        return ans;
+
+    }
+}
