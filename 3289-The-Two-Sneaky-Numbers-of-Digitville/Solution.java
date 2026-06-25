688e2dce54d90a9935e1b4be5598f0424113b6f3
Shashidhara K <shashidharak334@gmail.com>
2026-06-25 21:29:09 +0530

    3289. The Two Sneaky Numbers of Digitville
    [Keep coding]

---
+++ 3289-The-Two-Sneaky-Numbers-of-Digitville/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int arr[]=new int[2];
+        int i=0;
+        for(Integer key:map.keySet()){
+            if(map.get(key)==2){
+                arr[i++]=key;
+            }
+        }
+        return arr;
+    }
+}
