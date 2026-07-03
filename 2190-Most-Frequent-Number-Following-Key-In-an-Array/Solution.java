215917c8a4bd8228879bf883ab0b29edadab31b1
Shashidhara K <shashidharak334@gmail.com>
2026-07-03 19:39:20 +0530

    2190. Most Frequent Number Following Key In an Array
    [Keep coding, Hashing, Easy]

---
+++ 2190-Most-Frequent-Number-Following-Key-In-an-Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mostFrequent(int[] nums, int key) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == key) {
+                map.put(nums[i + 1], map.getOrDefault(nums[i + 1], 0) + 1);
+            }
+        }
+        int max = 0;
+        int maxele = 0;
+        for (Integer ele : map.keySet()) {
+            if (map.get(ele) > max) {
+                max = map.get(ele);
+                maxele = ele;
+            }
+        }
+        return maxele;
+    }
+}
