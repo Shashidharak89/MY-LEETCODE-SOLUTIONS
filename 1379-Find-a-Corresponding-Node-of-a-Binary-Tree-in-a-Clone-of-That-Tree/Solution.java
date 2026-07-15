a0e33994e1631b7ed640835293cbedba96d84db1
Shashidhara K <shashidharak334@gmail.com>
2026-07-15 20:38:01 +0530

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
    [Keep coding, Tree, BFS, Easy]

---
+++ 1379-Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(cloned);
+        while(!q.isEmpty()){
+            TreeNode temp=q.poll();
+            if(temp.val==target.val){
+                return temp;
+            }
+            if(temp.left!=null){
+                q.add(temp.left);
+            }
+            if(temp.right!=null){
+                q.add(temp.right);
+            }
+        }
+        return cloned;
+
+    }
+}
