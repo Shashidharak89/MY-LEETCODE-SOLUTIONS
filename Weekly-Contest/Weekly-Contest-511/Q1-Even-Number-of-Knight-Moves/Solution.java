86dc5f9f1ffc2a40bb5c2a92d7fc20abf4f64a70
Shashidhara K <shashidharak334@gmail.com>
2026-07-19 08:41:55 +0530

    Q1. Even Number of Knight Moves
    [Keep coding, Weekly contest 511, Chess board, Easy]

---
+++ Weekly-Contest/Weekly-Contest-511/Q1-Even-Number-of-Knight-Moves/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canReach(int[] start, int[] target) {
+        int s = start[0] + start[1];
+        int e = target[0] + target[1];
+
+        if (s % 2 == e % 2) {
+            return true;
+        }
+        return false;
+    }
+}
